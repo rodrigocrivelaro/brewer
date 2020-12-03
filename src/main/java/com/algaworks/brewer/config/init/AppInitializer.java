ae3b21b54f467ce7ab753c25b9f3a1aa25fee50e
Rodrigo Crivelaro <rreiscrivelaro@gmail.com>
2020-12-03 14:31:39 -0300

    Resolvendo problemas de acentuação e aperfeiçoando a validação de formulários com o BeanValidator;

--- src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
+++ src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -1,5 +1,8 @@
 package com.algaworks.brewer.config.init;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.algaworks.brewer.config.WebConfig;
@@ -20,5 +23,14 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
 	}
-
+	
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return new Filter[] { characterEncodingFilter };
+	}
 }
--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -3,29 +3,32 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
-	
+
 	@RequestMapping("/cervejas/novo")
 	public String novo() {
 		return "cervejas/CadastroCerveja";
 	}
-	
+
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
-		
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+
 		if (result.hasErrors()) {
-			System.out.println(">>> Opa tem erro aqui...");
-		}
-		
-		System.out.println(">>>>>> SKU.." + cerveja.getSku() + " e o nome..: " + cerveja.getNome());
-		return "cervejas/CadastroCerveja";
+			model.addAttribute("mensagem", "Erro no formulário!");
+			return "cervejas/CadastroCerveja";
+		} 
+
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");		
+		return "redirect:/cervejas/novo";
 	}
-	
+
 }
