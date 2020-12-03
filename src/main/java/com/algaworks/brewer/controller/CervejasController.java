7337273005f067e62dd0be7e8f3a768c5289fbee
Rodrigo Crivelaro <rreiscrivelaro@gmail.com>
2020-12-03 13:44:36 -0300

    Implementando a validação do formulário no projeto com BeanValidator;

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,7 +1,13 @@
 package com.algaworks.brewer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.algaworks.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
@@ -10,4 +16,16 @@ public class CervejasController {
 	public String novo() {
 		return "cervejas/CadastroCerveja";
 	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			System.out.println(">>> Opa tem erro aqui...");
+		}
+		
+		System.out.println(">>>>>> SKU.." + cerveja.getSku() + " e o nome..: " + cerveja.getNome());
+		return "cervejas/CadastroCerveja";
+	}
+	
 }
